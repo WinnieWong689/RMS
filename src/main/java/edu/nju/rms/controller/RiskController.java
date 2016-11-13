f094f1a2b3fc2d5a308dd4161fc1ad2d06bf6348
JaneLdq <janeldq@163.com>
2016-11-13 21:11:30 +0800

    spring dao config

--- src/main/java/edu/nju/rms/controller/RiskController.java
+++ src/main/java/edu/nju/rms/controller/RiskController.java
@@ -1,11 +1,10 @@
 package edu.nju.rms.controller;
 
-import java.util.List;
 import java.io.IOException;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,18 +13,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import edu.nju.rms.interceptor.Auth;
 import edu.nju.rms.interceptor.Role;
-import edu.nju.rms.model.RiskProject;
 import edu.nju.rms.service.RiskService;
 
 @Controller
 @RequestMapping(value="")
 public class RiskController {
 	
-//	private RiskService riskService;
-//
-//	public void setRiskService(RiskService riskService) {
-//		this.riskService = riskService;
-//	}
+	private RiskService riskService;
+
+	public void setRiskService(RiskService riskService) {
+		this.riskService = riskService;
+	}
 
 	@Auth(Role.USER)
 	@RequestMapping(value="/risk/risk_track_list", method=RequestMethod.GET)
