c3583639b870bcd86dbeffea89afc11b0eeeb978
Katharine <sunmengxi19951207@icloud.com>
2016-11-13 22:57:17 +0800

    add project depend on the role of the user

---
+++ src/main/webapp/js/project.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+
+//	$.ajax({
+//		type: "get",
+//		url: "/rms/user/delete",
+//		data: {
+//			ids: ids
+//		},
+//		success: function(data){
+//			if (data.result) {
+//				window.location.href = "/rms/user";
+//			}
+//		},
+//		complete: function() {
+//			$('#del-confirm-modal').modal('hide');
+//		}
+//	});
+//
+//	$('#btn-search').click(function() {
+//		$('#search-form').submit();
+//	});
+//
+//	function updateDeleteBtnStatus() {
+//		var selections = $('input[name="user-selector"]:checked');
+//		if (selections.length > 0) {
+//			$('#js-delete-btn').prop("disabled", false);
+//		} else {
+//			$('#js-delete-btn').prop("disabled", true);
+//		}
+//	}
+})
